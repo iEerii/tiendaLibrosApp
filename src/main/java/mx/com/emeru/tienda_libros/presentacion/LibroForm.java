25a6f44e1757b0dd472dd8edb27127dc8d14d815
Eri <erickaponce92@gmail.com>
2024-03-11 12:13:54 -0500

    add agregarLibro method to add books to DB

--- src/main/java/mx/com/emeru/tienda_libros/presentacion/LibroForm.java
+++ src/main/java/mx/com/emeru/tienda_libros/presentacion/LibroForm.java
@@ -1,5 +1,6 @@
 package mx.com.emeru.tienda_libros.presentacion;
 
+import mx.com.emeru.tienda_libros.modelo.Libro;
 import mx.com.emeru.tienda_libros.servicio.LibroServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -26,9 +27,7 @@ public class LibroForm extends JFrame {
     public LibroForm(LibroServicio libroServicio){
         this.libroServicio = libroServicio;
         iniciarForma();
-        agregarButton.addActionListener(e -> {
-
-        });
+        agregarButton.addActionListener(e -> agregarLibro());
     }
 
     public void iniciarForma(){
@@ -41,6 +40,36 @@ public class LibroForm extends JFrame {
         int x = (tamanhoPantalla.width - getWidth()/2);
         int y = (tamanhoPantalla.height - getHeight()/2);
         setLocation(x, y);
+    }
+
+    private void agregarLibro(){
+        //Leer los valores del formulario
+        if(libroTexto.getText().equals("")){
+            mostrarMensaje("Proporciona el nombre del libro");
+            libroTexto.requestFocusInWindow();
+            return;
+        }
+        var nombreLibro = libroTexto.getText();
+        var autor = libroTexto.getText();
+        var precio = Double.parseDouble(precioTexto.getText());
+        var existencias = Integer.parseInt(existenciasTexto.getText());
+        //Se crea el objeto de libro
+        var libro = new Libro(null, nombreLibro, autor, precio, existencias);
+        this.libroServicio.guardarLibro(libro);
+        mostrarMensaje("Se agrego el libro...");
+        limpiarFormulario();
+        listarLibros();
+    }
+
+    private void limpiarFormulario(){
+        libroTexto.setText("");
+        autorTexto.setText("");
+        precioTexto.setText("");
+        existenciasTexto.setText("");
+    }
+
+    private void mostrarMensaje(String mensaje){
+        JOptionPane.showMessageDialog(this, mensaje);
     }
 
     private void createUIComponents() {
